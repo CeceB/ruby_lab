8ae93cde562b3e8818d533e6b91dbf31d470274b
Cecelia <cecelia.batt@gmail.com>
2015-07-30 13:35:00 -0400

    paired up students

--- student_teams.rb
+++ student_teams.rb
@@ -2,4 +2,13 @@ students = ["Megan","Eamon","Keith","Ryan","Brian","Cecelia","Brant","Postal"]
 
 students.shuffle!
 
-puts students
+#working with an array we need a count, working with indexes
+count = 0
+
+until count == students.length
+#while count is < students.length
+	
+	puts "#{students[count]}&#{students[count+1]}"
+
+	count += 2 #looping through an increasing amount changed 1 to 2 so that people were not repeated
+end 
